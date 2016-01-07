080437e853a42563ad55b8333f8e2148a9259e6b
zanjs <root@zanjs.com>
2016-01-07 18:19:01 +0800

    :cat: taobao :wangwang

--- 20160105/src/js/main.js
+++ 20160105/src/js/main.js
@@ -7,21 +7,41 @@
    //touchcancel:        
   
   PanLi(function(){
-        var _hostName = window.location.hostname;
-     
-    
-     
-    PanLi(".action-bar").html('');
-    
-    
-    PanLi(".action-bar,#detail-base-smart-banner").remove();
       
-     PanLi("body").on("touchend click touchmove touchstart",".mask-top,.mask-bottom",function(event){
-          console.log("阻止");
-          event.stopPropagation();          
-          return false;
-        
-     });      
+      
+          var _hostName = window.location.hostname;    
+     
+          PanLi(".action-bar").html('');
+          
+          
+          PanLi(".action-bar,#detail-base-smart-banner,.tryme,.cart-concern-btm-fixed,.item-action").remove();
+          
+          
+          console.log(_hostName);
+          
+          if(_hostName == 'h5.m.taobao.com'){
+              console.log("==h5.m.taobao.com")
+              PanLi('body').css({'paddingTop':'0'});             
+              
+          } 
+          
+          
+          if(_hostName == 'detail.m.tmall.com'){
+            PanLi("#content").css({'paddingTop':'0'});
+          }
+          
+          
+          setTimeout(function(){
+            var taobaoClose = PanLi("[id$='-close']");
+            console.log(taobaoClose.html());			
+            taobaoClose.parent().remove();
+            if(_hostName == 'h5.m.taobao.com'){
+              console.log("==h5.m.taobao.com")
+              PanLi('body').css({'paddingTop':'0'});
+            }
+          },500)
+      
+      
   })
      
      
