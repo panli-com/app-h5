9ff9440333a4434bb87050fd40d9bdeb64f94cfe
zanjs <root@zanjs.com>
2016-01-13 11:59:38 +0800

    :anguished: - 增加遗漏的 http 协议的 taobao.com By gitpush

--- 20160105/src/js/c_function.js
+++ 20160105/src/js/c_function.js
@@ -3,30 +3,10 @@ function appV(){
   return "0.0.6";
 }
 
-//获取服务端数据   
-function getSeverData(url,obj,callback) {
-   
-     PanLi.ajax({
-            type: "get",
-            url: url,
-            dataType: "json",
-            data: obj,
-            cache: false,
-            contentType: "application/json;utf-8",
-            timeout: 20000,
-            error: function () {
-                console.log('请求错误，请再试');              
-               
-            },
-            success: function (data) {
-                callback(data);
-            }
-        });    
-}
 
-// 获取域名
+// 获取域名 
 function domainURI(str){
-		var durl=/https?:\/\/(?:[^/]+\.)?([^./]+\.(?:cn|com|top))(?:PanLi|\/)/;
+		var durl=/https?:\/\/(?:[^/]+\.)?([^./]+\.(?:com.cn|cn|com|net|top))(?:PanLi|\/)/;
 		var domain = str.match(durl); 
 		return domain[1];
  }
@@ -37,62 +17,7 @@ function domainURI2(str){
 		var durl= /(\w*\.\w*\.(?:com|cn|top)).*/;
 		var domain = str.match(durl); 
 		return domain[1];
- }
-
-//中国地址数据响应		
-function chinaAddressReact(obj){				
-    var data = obj['china'];
-    console.log(data);
-    if(data){
-       maskStyleReact(data); 
-    }else{
-        alert("返回数据错误")
-    }
-    
 }
 
-
-//国外地址数据响应	
-function noChinaAddressReact(obj){		
-    
-    var status = obj['status'];
-    var data = '';
-    console.log(status);
-    if(status == 0){
-        data = obj['nochina'];
-    }else{
-        data = obj['china'];
-    };	
-    maskStyleReact(data);
-    
-}
-
-//遮罩层页面响应
-function maskStyleReact(data){      
-        
-        var styleC = 'position: fixed;left:0;right:0;width:100%;background:#FFF;';
-        var html = '<div class="mask-bottom" style="'+ styleC +'"></div><div class="mask-top" style="'+ styleC +'"></div>';       
-        
-        PanLi("body").append(html);             
-        PanLi(".mask-top").css(data['topmask']);
-        PanLi(".mask-bottom").css(data['bottommask']);
-        
-}
-
-
-
-
-function addressReact(api,iframeSrc,address) {
-    var obj = { url: domainURI2(iframeSrc) };
-            // 获取域名遮罩 层信息
-    getSeverData(api, obj, function (data) {
-        console.log(data);     
-        if (address == '国内用户') {
-            chinaAddressReact(data);
-        } else {
-            noChinaAddressReact(data);
-        }
-    })
-}
 
 
--- 20160105/src/js/main.js
+++ 20160105/src/js/main.js
@@ -1,5 +1,4 @@
-;(function(){
-  
+;(function(){ 
   
    //touchstart:     
    //touchmove:     
@@ -21,24 +20,14 @@
           if(_hostName == 'localhost'){
               url = 'taobao.com';
           }else{
-              url = domainURI(window.location.href);
+              url = tldjs.getDomain(window.location.href);
           }
           
           
-          var getUrl = 'http://172.20.7.232:5029/H5API/H5DomControl?url='+url;
-          var obj = '';
           
-         
-
+          console.log(PLElements[url]);
           
-          getSeverData(getUrl,obj,function(data){
-             
-              var elements = data[0].DOMS;
-               PanLi(elements).remove();
-               console.log('执行完成');
-              
-          })
-           
+          PanLi(PLElements[url]).remove();
           
           if(_hostName == 'h5.m.taobao.com'){
               console.log("竟然是手淘 == h5.m.taobao.com")
@@ -65,19 +54,7 @@
       
   })
      
-     
-   
-     
-     
-      //    var src = 'http://h5.m.taobao.com/awp/core/detail.htm?spm=a21c0.34764.138697.1&id=524047443564';
-      // var api = 'http://172.20.7.232:5029/H5API/GetH5Cover';
-      //api：服务器请求接口地址
-      // src :厂家地址
-      // 客户地址信息：国内用户 和 非
-      //  addressReact(api,src,'国内用户'); 
-     
-    
-     
+ 
 
     
  
