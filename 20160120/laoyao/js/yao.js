4c57b4b34cfc65c440b8ae1fb2272f7c373694bd
zanjs <root@zanjs.com>
2016-01-20 15:17:10 +0800

    :green_heart: - HTML5 手机摇一摇 By gitpush

---
+++ 20160120/laoyao/js/yao.js
@@ -0,0 +1,24 @@
+
+var num = 0;
+
+function doShake(){
+	$("#X_do").removeClass("doShake_a").addClass("doShake");
+	$("#music").html('<EMBED src="images/m.mp3" autostart="true" loop="true" width="80" height="20" hidden="true" >');
+}
+if(window.DeviceMotionEvent) {
+	var speed = 25;
+	var x = y = z = lastX = lastY = lastZ = 0;
+	window.addEventListener('devicemotion', function(){
+		var acceleration =event.accelerationIncludingGravity;
+		x = acceleration.x;
+		y = acceleration.y;
+		if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed) {
+			if(num == 0){
+				doShake();
+				num = 1;
+			}
+		}
+		lastX = x;
+		lastY = y;
+	}, false);
+}
